1429927614f35a055ce8afe9576bd22d8e9c34ae
Key Firdausi Alfarel <144311812+farelsukabelajar@users.noreply.github.com>
2024-06-28 23:27:12 +0700

    Delete F-BJV24001115-synrgy7-key-binfood-ch4/binarfud directory

--- F-BJV24001115-synrgy7-key-binfood-ch4/binarfud/src/main/java/com/example/binarfud/repository/MerchantRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.binarfud.repository;
-
-import com.example.binarfud.model.Merchant;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface MerchantRepository extends JpaRepository<Merchant, UUID> {
-}
--- F-BJV24001115-synrgy7-key-binfood-ch4/binarfud/src/main/java/com/example/binarfud/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.binarfud.repository;
-
-import com.example.binarfud.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface UserRepository extends JpaRepository<User, UUID> {
-}
